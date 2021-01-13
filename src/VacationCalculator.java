8e923ab9c8492a65d67a655cc73558d23c0b6357
Jamie Hurd <jaoshu@bitbucket.org>
2021-01-12 21:02:31 -0700

    tally improved

--- src/VacationCalculator.java
+++ src/VacationCalculator.java
@@ -1,15 +1,42 @@
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Iterator;
 
 public class VacationCalculator
 {
+    Destination d1 = Destination.Mexico;
+    Destination d2 = Destination.Europe;
+    Destination d3 = Destination.Japan;
+    float totalCost = 0;
+    float addOn = 0;
     HashMap<Destination, Float> vacationMap = new HashMap<Destination, Float>();
-    // git  test
+
+    /*******************************
+     * TALLYEXPENSES
+     * @param vacationMap
+     * @return totalCost
+     ******************************/
     float tallyExpenses(HashMap<Destination, Float> vacationMap)
     {
+        Iterator vacationIterator = vacationMap.entrySet().iterator();
+        while (vacationIterator.hasNext())
+        {
+            Map.Entry mapElement = (Map.Entry)vacationIterator.next();
+            addOn = ((float)mapElement.getValue());
+        }
+        return totalCost += addOn;
+    }
+
+    public float calculateVacationCost(Destination d)
+    {
+        Cruise c = new Cruise(d);
+        if()
+        {
+
+        }
+
         this.vacationMap = vacationMap;
-        Destination d1 = Destination.Mexico;
-        Destination d2 = Destination.Europe;
-        Destination d3 = Destination.Japan;
+
         if (vacationMap.equals(d1))
         {
             return vacationMap.get(d1);
@@ -22,15 +49,13 @@ public class VacationCalculator
         }
     }
 
-    public float calculateVacationCost(Destination d)
-    {
-        float cost = 0;
-        return cost;
-    }
-
     public static void main (String[] args)
     {
+        Destination d1 = Destination.Mexico;
+        Destination d2 = Destination.Europe;
+        Destination d3 = Destination.Japan;
         VacationCalculator vc = new VacationCalculator();
         float japanCost = vc.calculateVacationCost(Destination.Japan);
+        System.out.format("The cost of a vacation to Japan comprises: %f%n", japanCost);
     }
 }
