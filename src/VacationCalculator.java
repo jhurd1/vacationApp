c635368314d1a01d1076df4b681272ca21a42912
Jamie Hurd <jaoshu@bitbucket.org>
2021-01-12 21:27:51 -0700

    need to pass hashMap values

--- src/VacationCalculator.java
+++ src/VacationCalculator.java
@@ -7,8 +7,8 @@ public class VacationCalculator
     Destination d1 = Destination.Mexico;
     Destination d2 = Destination.Europe;
     Destination d3 = Destination.Japan;
-    float totalCost = 0;
-    float addOn = 0;
+    private float totalCost = 0;
+    private float addOn = 0;
     HashMap<Destination, Float> vacationMap = new HashMap<Destination, Float>();
 
     /*******************************
@@ -18,35 +18,37 @@ public class VacationCalculator
      ******************************/
     float tallyExpenses(HashMap<Destination, Float> vacationMap)
     {
+        // tallyExpenses iterates across the hashMap
         Iterator vacationIterator = vacationMap.entrySet().iterator();
+
+        // Use the iterator to traverse the hashMap
         while (vacationIterator.hasNext())
         {
             Map.Entry mapElement = (Map.Entry)vacationIterator.next();
+
+            // Hold the values in addOn
             addOn = ((float)mapElement.getValue());
         }
+
+        // return and assign the totalCost to the addOn
         return totalCost += addOn;
     }
 
+    /*******************************
+     * CALCULATEVACATIONCOST
+     * @param d
+     * @return totalCost
+     ******************************/
     public float calculateVacationCost(Destination d)
     {
-        Cruise c = new Cruise(d);
-        if()
-        {
+        // totalCost holds a call to tallyExpenses
+        totalCost = tallyExpenses(vacationMap);
 
-        }
+        // Put the destination and totalCost in the hashMap
+        vacationMap.put(d, totalCost);
 
-        this.vacationMap = vacationMap;
-
-        if (vacationMap.equals(d1))
-        {
-            return vacationMap.get(d1);
-        } else if (vacationMap.equals(d2))
-        {
-            return vacationMap.get(d2);
-        } else
-        {
-            return vacationMap.get(d3);
-        }
+        // return the totalCost
+        return totalCost;
     }
 
     public static void main (String[] args)
@@ -54,8 +56,9 @@ public class VacationCalculator
         Destination d1 = Destination.Mexico;
         Destination d2 = Destination.Europe;
         Destination d3 = Destination.Japan;
+        Cruise c = new Cruise(d3);
         VacationCalculator vc = new VacationCalculator();
-        float japanCost = vc.calculateVacationCost(Destination.Japan);
+        float japanCost = vc.calculateVacationCost(d3);
         System.out.format("The cost of a vacation to Japan comprises: %f%n", japanCost);
     }
 }
